0fba56a5f666ad3f475ca2601bd9ac89a1d1f449
echen <98108241+echen642@users.noreply.github.com>
2022-04-26 17:05:06 -0500

    Added blocks onto the screen.

--- block_manager.py
+++ block_manager.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 
 class BlockManager:
-    def __init__(self):
-        pass
+    def __init__(self, canvas):
+        canvas.grid(row=10, column=0)
+        canvas.create_rectangle(10, 10, 100, 100, fill = "green")
+        canvas.grid(row=20, column=0)
+        canvas.create_rectangle(110, 110, 200, 200, fill = "green")
     
--- wordle.py
+++ wordle.py
@@ -1,13 +1,18 @@
 import tkinter as tk
+from block_manager import BlockManager
 
 class Wordle:
     def __init__(self):
         self.mainWin = tk.Tk()
+        canvas = tk.Canvas(self.mainWin, width = 500, height = 800)
+
         self.mainWin.title("Wordle")
         self.mainWin.geometry("500x800")
+        self.blockManager = BlockManager(canvas)
 
     def run(self):
         self.mainWin.mainloop()
+        
 
 w = Wordle()
 w.run()
