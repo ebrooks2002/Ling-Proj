dcfb1e78a9357b787970f5a1044143af2586fe98
ebrooks2002 <84427247+ebrooks2002@users.noreply.github.com>
2022-04-29 00:57:54 -0500

    hg

--- wordle.py
+++ wordle.py
@@ -9,7 +9,7 @@ from setuptools import Command
 from words import Words
 
 class Wordle: 
-    w = Words()
+    words = Words()
     valid_words = None
     correctWord = ""
     
@@ -17,9 +17,8 @@ class Wordle:
         self.mainWin = tk.Tk()
         self.mainWin.title("Wordle")
         self.mainWin.geometry("400x800")
-        #self.canvas = Canvas(self.mainWin)
         self.listEntry = []
-        # Use state = DISABLED after a row has been completed
+
         for row in range(6):
             for column in range(4):
                 self.var = StringVar()
@@ -59,7 +58,7 @@ class Wordle:
     def buttonCallBacks(self, rowNumber):
         print("please work")
         word = self.getRow(rowNumber)
-        if self.w.contains(word) == False:
+        if self.words.contains(word) == False:
             print("not a word")
         self.compareWord(word, rowNumber)
 
